88359be37d0d48908aab8dbc56d35a09e565914b
mariannacoppa <densimetra@gmail.com>
2024-09-24 17:18:09 +0200

    [UPD] created file home.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc-logo">
                 </div>
             </div>
         </div>
