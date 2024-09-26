c22959971df1c06a3bde542d3ab09493ece06071
mariannacoppa <densimetra@gmail.com>
2024-09-26 18:49:35 +0200

    [UPD] cycled comic cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,11 +11,22 @@
             </div>
         </div>
     </div>
-    <div id="container">
+    <div id="comics-container">
         <div class="row">
             <div class="col-12">
-                <h2 class="text-center">FUMETTI QUI</h2>
             </div>
+        </div>
+        <div class="row">
+            @foreach ($comics as $comic)
+            <div class="col-12 col-md-3 col-lg-2">
+                <div class="comic-card m-1 border-0">
+                    <img class="img-fluid" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                    <div class="card-body">
+                        <span class="text-uppercase">{{ $comic['title'] }}</span>
+                    </div>
+                </div>
+            </div>
+            @endforeach
         </div>
     </div>
 </main>
