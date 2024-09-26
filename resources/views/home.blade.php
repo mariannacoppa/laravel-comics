fbbbd262bbebc83f08ad963ad9fe3bb74ce483bd
mariannacoppa <densimetra@gmail.com>
2024-09-26 17:01:48 +0200

    [UPD] added main macro-structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,22 @@
-{{-- @extends('layouts.app') --}}
+@extends('layouts.app')
 
 @section('content')
 <main>
     <div id="jumbotron">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Jumbotron</h2>
+                </div>
+            </div>
+        </div>
     </div>
-    <div id="comics-container">
-
+    <div id="container">
+        <div class="row">
+            <div class="col-12">
+                FUMETTI QUI
+            </div>
+        </div>
     </div>
 </main>
 @endsection
